762166c88b25020b11def829a15a6f3f11c27503
issy20 <gggibraltar86@gmail.com>
2022-04-12 15:56:50 +0900

    to pwa

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
 import { RecoilRoot } from 'recoil'
+import { registerSW } from 'virtual:pwa-register'
 
 ReactDOM.render(
   <RecoilRoot>
@@ -10,3 +11,5 @@ ReactDOM.render(
   </RecoilRoot>,
   document.querySelector('#root')
 )
+
+registerSW()
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  root: "./",
-  plugins: [reactRefresh()],
+  root: './',
+  plugins: [reactRefresh(), VitePWA()],
   resolve: {
     alias: {
       '@/': path.join(__dirname, './src/'),
