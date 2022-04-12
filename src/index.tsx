3a6a10a611cc2aae807c3bf28bee5a5e87be4634
issy20 <gggibraltar86@gmail.com>
2022-04-12 15:44:37 +0900

    arragement module

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 import './index.css'
-import Calculator from './components/page/Calculator'
-import Home from './components/page/Home'
 import { App } from './App'
 import { RecoilRoot } from 'recoil'
 
